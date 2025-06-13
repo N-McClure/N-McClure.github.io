9fd73842177988c178a86d33825a2b2c97bfe42d
Nick McClure <64433966+N-McClure@users.noreply.github.com>
2025-06-13 08:31:54 -0400

    added all the images to the image array list

--- Project_Files/David/script.js
+++ Project_Files/David/script.js
@@ -4,21 +4,36 @@ console.log("Connected...Running...");
 //The Array containing the Images:
 let images = [
     "multimedia/im1.jpg",
+    "multimedia/im2.jpg",
     "multimedia/im3.jpg",
     "multimedia/im4.jpg",
     "multimedia/im5.jpg",
     "multimedia/im6.jpg",
+    "multimedia/im7.jpg",
     "multimedia/im8.jpg",
+    "multimedia/im9.jpg",
     "multimedia/im10.jpg",
+    "multimedia/im11.jpg",
+    "multimedia/im12.jpg",
+    "multimedia/im13.jpg",
     "multimedia/im14.jpg",
     "multimedia/im15.jpg",
+    "multimedia/im16.jpg",
+    "multimedia/im17.jpg",
     "multimedia/im18.jpg",
+    "multimedia/im19.jpg",
     "multimedia/im20.jpg",
     "multimedia/im21.jpg",
     "multimedia/im22.jpg",
     "multimedia/im23.jpg",
+    "multimedia/im24.jpg",
     "multimedia/im25.jpg",
-    "multimedia/im26.jpg"
+    "multimedia/im26.jpg",
+    "multimedia/im27.jpg",
+    "multimedia/im18.png",
+    "multimedia/im29.jpg",
+    "multimedia/im30.jpg",
+    "multimedia/im31.jpg",
 
 ];
 
